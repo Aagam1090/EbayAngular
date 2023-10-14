79efda78678d55ed6f3648f398b0a2028366a87b
Aagam1090 <aagamshah1090@gmail.com>
2023-10-14 11:17:50 -0700

    Fixed UI + Clear issue

--- src/app/components/ebay-form/ebay-form.component.ts
+++ src/app/components/ebay-form/ebay-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-ebay-form',
@@ -6,17 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./ebay-form.component.css']
 })
 export class EbayFormComponent {
+  keyword:string = '';
+  selectedCategory:string = "All Categories"
+  displayKeywordError = false;
+  displayLocationError = false;
+  distance:number = 10;
 
-  displayFormError = false;
+  get isValidKeyword(): boolean {
+    return ( this.keyword!= null && this.keyword != '' );
+  }
 
-  getData(data:any){
-    data = data.form.value;
-    if(data.keyword.trim().length==0){
-      this.displayFormError = true;
+  getData(form: NgForm): void {
+    this.displayKeywordError = !this.validateKeyword(form.value.keyword);
+    this.displayLocationError = !this.validateLocation(form.value.CurrentLocation, form.value.zip);
+    
+    if(this.displayKeywordError || this.displayLocationError) {
+        return;
     }
-    console.log(data);
-    console.log(this.displayFormError);
+    // if validations pass, perform your data submission logic here...
+    console.log('Form data submitted: ', form.value);
+  }
+  
+  validateKeyword(keyword: string): boolean {
+    return Boolean(keyword) && keyword.trim() !== '';
+  }
 
+  validateLocation(location: string, zip?: string): boolean {
+    if (location === 'Other' && (!zip || zip.trim() === '')) {
+      this.displayLocationError = true;
+      return false;
+    }
+    this.displayLocationError = false;
+    return location === 'CurrentLocation' || location === 'Other';
+  }
+
+  resetForm(form: NgForm): void {
+    form.resetForm();
+    setTimeout(() => {
+      this.selectedCategory = "All Categories";
+      this.distance = 10;
+      this.displayKeywordError = false;
+      this.displayLocationError = false;
+    });
   }
 
 }

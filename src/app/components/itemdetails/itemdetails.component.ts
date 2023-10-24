5a54fce19a9a76230c024d7a82c4dddd935f5830
Aagam1090 <aagamshah1090@gmail.com>
2023-10-24 11:04:49 -0700

    Fixed CSS for Button Active/InActive

--- src/app/components/itemdetails/itemdetails.component.ts
+++ src/app/components/itemdetails/itemdetails.component.ts
@@ -1,31 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-itemdetails',
   templateUrl: './itemdetails.component.html',
   styleUrls: ['./itemdetails.component.css']
 })
-export class ItemdetailsComponent {
+export class ItemdetailsComponent{
   @Input() singleItem: any = [];
+  activeContent: string = 'Product'; 
 
-  content = "";
-  renderContent(data:any){
-    switch(data){
-      case "Product":
-        this.content = "Product";
-        break;
-      case "Photo":
-        this.content = "Photo";
-        break;
-      case "Shipping":
-        this.content = "Shipping";
-        break;
-      case "Seller":
-        this.content = "Seller";
-        break;
-      case "Similar":
-        this.content = "Similar";
-        break;
-    }
+  renderContent(content: string) {
+    this.activeContent = content;
+  }
+
+  isActive(content: string): boolean {
+    return this.activeContent === content;
   }
 }

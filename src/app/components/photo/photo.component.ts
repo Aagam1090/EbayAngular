9169fb9dcb5d1f8724844b10fa7a3e45fa513664
Aagam Bharat Shah <aagamshah1090@gmail.com>
2023-11-04 23:54:54 -0700

    Completed Similar Products

--- src/app/components/photo/photo.component.ts
+++ src/app/components/photo/photo.component.ts
@@ -18,18 +18,11 @@ export class PhotoComponent {
     this.fetchPhotos(this.item.Title).subscribe(response => {
       this.photosData = response;
       console.log('PhotosData API Data',this.photosData);
-      this.loopImages();
       this.modifiedPhotosData = [...this.photosData];
       if (this.modifiedPhotosData.length > 6) {
         this.modifiedPhotosData.splice(6, 0, null);
       }
     }); 
-  }
-
-  loopImages(){
-    for(let i=0;i<this.photosData.length;i++){
-      console.log(this.photosData[i].link);
-    }
   }
 
   fetchPhotos(title:string){
--- src/app/components/simproduct/simproduct.component.ts
+++ src/app/components/simproduct/simproduct.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-simproduct',
@@ -9,15 +9,56 @@ import { Component, Input } from '@angular/core';
 export class SimproductComponent {
   @Input() product: any;
   similarProducts: any = [];
+  similarProductsCopy: any = [];
+  sortCriteria: string = 'Default';
+  sortOrder: string = 'Ascending';
+  showMore: boolean = false;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  ngOnChanges(){
+  ngDoCheck()	{
+    if(this.similarProductsCopy.length == 0 && this.similarProducts.length > 0){
+      this.similarProductsCopy = [...this.similarProducts];
+      console.log('SimilarProductsCopy',this.similarProductsCopy);
+    }
+    if(this.sortCriteria == 'Default' && this.similarProductsCopy.length > 0){
+      this.similarProducts = [...this.similarProductsCopy];
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
     console.log('Similar Products Component',this.product);
     this.fetchSimilarProducts(this.product.ItemID).subscribe(response => {
       this.similarProducts = response;
       console.log('Similar Products API Data',this.similarProducts);
     }); 
+    if (changes['sortCriteria'] && changes['sortCriteria'].currentValue === 'Default') {
+      this.resetToOriginal();
+    } else {
+      this.sortingSimilarProducts();
+    }
+  }
+
+  sortingSimilarProducts(): void {
+    if (this.sortCriteria !== 'Default') {
+      this.similarProducts.sort((a: any, b: any) => {
+        let comparison = 0;
+        if (this.sortCriteria === 'Product Name') {
+          comparison = a.title.localeCompare(b.title);
+        } else if (this.sortCriteria === 'Days Left') {
+          comparison = this.getDaysLeft(a) - this.getDaysLeft(b);
+        } else if (this.sortCriteria === 'Price') {
+          comparison = a.buyItNowPrice.__value__ - b.buyItNowPrice.__value__;
+        } else if (this.sortCriteria === 'Shipping Cost') {
+          comparison = a.shippingCost.__value__ - b.shippingCost.__value__;
+        }
+        return this.sortOrder === 'Descending' ? -comparison : comparison;
+      });
+    }
+  }
+
+  resetToOriginal(): void {
+    this.similarProducts = [...this.similarProductsCopy];
   }
 
   fetchSimilarProducts(id:any){
@@ -25,11 +66,26 @@ export class SimproductComponent {
     return this.http.get('http://localhost:3000/similar?itemId='+id);
   }
 
-  getDaysLeft(product:any): string {
+  getDaysLeft(product:any): number {
     let timeLeft = product.timeLeft;
     let startIndex = timeLeft.indexOf('P') + 1; 
     let endIndex = timeLeft.indexOf('D');
-    let daysLeft = timeLeft.substring(startIndex, endIndex);
+    let daysLeft = parseInt(timeLeft.substring(startIndex, endIndex),0);
     return daysLeft;
   }
+
+  getDisplayedProducts() {
+    if(this.showMore == true){
+      return this.similarProducts;
+    }
+    else{
+      return this.similarProducts.slice(0, 5);
+    }
+  }
+
+  toggleShowMore(){
+    this.showMore = !this.showMore;
+    console.log('Show More',this.showMore);
+  }
+  
 }

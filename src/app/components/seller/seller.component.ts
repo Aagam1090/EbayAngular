51b8da2f1fbeb10fa171eac836a4ad8b670169f3
Aagam Bharat Shah <aagamshah1090@gmail.com>
2023-11-04 22:41:04 -0700

    Fixed Seller Icon

--- src/app/components/seller/seller.component.ts
+++ src/app/components/seller/seller.component.ts
@@ -15,18 +15,48 @@ export class SellerComponent {
     this.current = this.item.Seller.PositiveFeedbackPercent;
     this.max=100;
   }
-  getStarColor(feedbackRatingStar: string): string {
-    switch(feedbackRatingStar) {
-      case 'Yellow': return '#FFD700';
-      case 'Blue': return '#0000FF';
-      case 'Turquoise': return '#40E0D0';
-      case 'Purple': return '#800080';
-      case 'Red': return '#FF0000';
-      case 'Green': return '#008000';
-      case 'Silver': return '#C0C0C0';
-      default: return '#000000';
+  getStarDetails(feedbackScore: number): { color: string, icon: string } {
+    console.log(feedbackScore);
+    let details = { color: '#000000', icon: 'star_border' }; // default values
+  
+    if (feedbackScore >= 10000) {
+      details.icon = 'stars'; // use filled star for scores 10000 and above
     }
+  
+    if (feedbackScore <= 9) {
+      details.color = ''; // No color for score 0-9, and no icon should be displayed
+    } else if (feedbackScore <= 49) {
+      details.color = '#FFD700'; // Yellow Star
+    } else if (feedbackScore <= 99) {
+      details.color = '#0000FF'; // Blue Star
+    } else if (feedbackScore <= 499) {
+      details.color = '#40E0D0'; // Turquoise Star
+    } else if (feedbackScore <= 999) {
+      details.color = '#800080'; // Purple Star
+    } else if (feedbackScore <= 4999) {
+      details.color = '#FF0000'; // Red Star
+    } else if (feedbackScore <= 9999) {
+      details.color = '#008000'; // Green Star
+    } else if (feedbackScore >= 10000) {
+      // For scores 10000 and above, already set the icon to 'star'
+      if (feedbackScore <= 24999) {
+        details.color = '#FFD700'; // Yellow Shooting Star
+      } else if (feedbackScore <= 49999) {
+        details.color = '#40E0D0'; // Turquoise Shooting Star
+      } else if (feedbackScore <= 99999) {
+        details.color = '#800080'; // Purple Shooting Star
+      } else if (feedbackScore <= 499999) {
+        details.color = '#FF0000'; // Red Shooting Star 245790
+      } else if (feedbackScore <= 999999) {
+        details.color = '#008000'; // Green Shooting Star
+      } else {
+        details.color = '#C0C0C0'; // Silver Shooting Star
+      }
+    }
+    console.log(details);
+    return details;
   }
+  
   getOverlayStyle() {
     const isSemi = false;
     const transform = (isSemi ? '' : 'translateY(-50%) ') + 'translateX(-50%)';
